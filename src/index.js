adf50f4d22ede15ee224b9e5cb893dbe1f42d77c
Alla Diachkova <lldckv@gmail.com>
2026-06-13 20:57:17 +0300

    fix: allow any origin and method

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 export default {
   async fetch(request) {
+    const corsHeaders = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,OPTIONS,DELETE",
+      "Access-Control-Allow-Headers": "*"
+    };
+ 
+    if (request.method === "OPTIONS") {
+      return new Response(null, { headers: corsHeaders });
+    }
+ 
     return new Response("lldckv", {
       headers: {
         "Content-Type": "text/plain; charset=utf-8",
         "X-Author": "lldckv",
-        "Access-Control-Allow-Origin": "*"
+        ...corsHeaders
       }
     });
   }
